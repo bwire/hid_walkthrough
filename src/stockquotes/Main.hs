2414a35ba6de3aa3109de27f3ad815d5d275bd56
Yevheniy Gandzyuck <e.barbedwire@gmail.com>
2019-07-12 14:01:34 +0300

    Describing data

--- src/stockquotes/Main.hs
+++ src/stockquotes/Main.hs
@@ -1,4 +1,24 @@
 module Main where
 
+data Params
+data QuoteData
+data QuoteDataCollection
+data StatInfo
+
+work :: Params -> IO()
+work = undefined
+
+readData :: FilePath -> IO QuoteDataCollection
+readData = undefined
+
+statInfo :: QuoteDataCollection -> StatInfo
+statInfo = undefined
+
+--statReport :: StatInfo -> Text
+--statReport = undefined
+
+plotCharts :: Params -> QuoteDataCollection -> IO()
+plotCharts = undefined
+
 main :: IO ()
 main = undefined
--- src/stockquotes/QuoteData.hs
+++ src/stockquotes/QuoteData.hs
@@ -1,11 +1,17 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module QuoteData (
   Fixed4,
   text2Quotes
 ) where
 
 import Data.Fixed (HasResolution (..), Fixed)
-import Data.Time (Day, defaultTimeLocale, parseTimeOrError)
+import Data.Time (Day, defaultTimeLocale, parseTimeOrError, parseTimeM)
 import qualified Data.Text as T
+import GHC.Generics (Generic)
+import Data.Csv (FromNamedRecord, FromField(..))
+import Safe(readDef)
+import Data.ByteString.Char8 (unpack)
 
 data E4
   
@@ -21,14 +27,29 @@ data QuoteData = QuoteData {
   open :: Fixed4,
   high :: Fixed4,
   low :: Fixed4
-}
+} deriving (Generic, FromNamedRecord)
 
---text2Quotes :: T.Text -> [QuoteData]
+instance FromField Day where
+  parseField = (parseTimeM False defaultTimeLocale "%Y/%m/%d") . unpack
+
+instance FromField Fixed4 where
+  parseField = pure . readDef 0 . unpack
+
+data QData = Open | Close | High | Low | Volume deriving (Show)
+
+field2Fun :: QData -> QuoteData -> Fixed4
+field2Fun Open = open
+field2Fun Close = close
+field2Fun High = high
+field2Fun Low = low
+field2Fun Volume = volume
+  
+text2Quotes :: T.Text -> [QuoteData]
 text2Quotes = map (mkQuote . toComponents) . tail . T.lines
   where toComponents = map T.unpack . T.split (==',') 
         mkQuote (d : rest@[_,_,_,_,_]) =
           let day = parseTimeOrError False defaultTimeLocale "%Y%m%d" d
-              [cl, vol, op, hi, lo] = map read rest
-          in QuoteData day cl vol op hi lo
+              [close, volume, open, high, low] = map read rest
+          in QuoteData{..}
         mkQuote _ = error "Incorrect daya format"
 
